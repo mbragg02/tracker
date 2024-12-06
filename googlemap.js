1a5408053fda2c45aaaae5fe478cd961717ff9dc
Michael Bragg <michael.bragg@fundingcircle.com>
2024-12-06 07:21:14 +0000

    Refactor Google Maps integration and improve HTML structure
    - Updated googlemap.js for better variable scoping and error handling
    - Enhanced JSON parsing with modern JavaScript features
    - Improved marker creation and infowindow display
    - Revised index.html for better semantics and added CSS file
    - Introduced style.css for consistent layout and styling

--- googlemap.js
+++ googlemap.js
@@ -1,71 +1,69 @@
-var map;
-var infowindow = new google.maps.InfoWindow();
-var bounds = new google.maps.LatLngBounds();
+let map;
+const infowindow = new google.maps.InfoWindow();
+const bounds = new google.maps.LatLngBounds();
 
 function initialize() {
-	var myLatlng = new google.maps.LatLng(54.361652,-1.424279);
-	var myOptions = {
-		 // zoom: 3,
-		center: myLatlng,
-		mapTypeId: google.maps.MapTypeId.TERRAIN
-	}
-	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-	jsonparse();
+    const myLatlng = new google.maps.LatLng(54.361652, -1.424279);
+    const myOptions = {
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    jsonparse();
 }
 
 function jsonparse() {
-	var locations = new Array(); 
-	$.getJSON('positions.json', function(data){ 
-	//	console.log(data)
-    $.each(data, function(i, position){ 	
-		var lat = data[i].latitude;
-		var lng = data[i].longitude;
-		var name = data[i].name;
-		var address = data[i].normal_name;
-		var located_at = data[i].located_at
-		locations.push([lat,lng, name, address, located_at]);
-	});
-	setMarkers(map, locations);
- // console.log(locations)
-	});
+    const locations = [];
+    $.getJSON('positions.json')
+        .done(data => {
+            data.forEach(position => {
+                const { latitude, longitude, name, normal_name, located_at } = position;
+                locations.push([latitude, longitude, name, normal_name, located_at]);
+            });
+            setMarkers(map, locations);
+        })
+        .fail((jqxhr, textStatus, error) => {
+            console.error("Request Failed: " + textStatus + ", " + error);
+        });
 }
 
 function setMarkers(map, locations) {
-	var polylinearray =[];
-	for (var i = 0; i < locations.length; i++) {
-		var position = locations[i];		
-		var positionLatLng = new google.maps.LatLng(position[0], position[1]);
-		polylinearray.push(positionLatLng);
-		var marker = createMarker(map, positionLatLng, position[2], position[3], position[4]);
-		bounds.extend(positionLatLng);
-		map.fitBounds(bounds);		
-	}
-//	console.log(polylinearray);
-	var bikeroute = new google.maps.Polyline({
-	    path: polylinearray,
-	    strokeColor: "#FF0000",
-	    strokeOpacity: 1.0,
-	    strokeWeight: 2
-	  });
-	bikeroute.setMap(map);
+    const polylinearray = [];
+    locations.forEach(position => {
+        const [lat, lng, name, address, located_at] = position;
+        const positionLatLng = new google.maps.LatLng(lat, lng);
+        polylinearray.push(positionLatLng);
+        const marker = createMarker(map, positionLatLng, name, address, located_at);
+        bounds.extend(positionLatLng);
+        map.fitBounds(bounds);
+    });
+
+    const bikeroute = new google.maps.Polyline({
+        path: polylinearray,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    bikeroute.setMap(map);
 }
 
 function createMarker(map, myLatLng, name, address, located_at) {
-	var dt = new Date(located_at);
-	var y = dt.getFullYear()
-	var m = dt.getMonth() + 1;
-	var d = dt.getDate()
-	//console.log(d)
-	var contentString = '<b>'+name+'</b><br />'+address+'<br />Located on: '+d+'-'+m+'-'+y+'</div>';	
+    const dt = new Date(located_at);
+    const y = dt.getFullYear();
+    const m = dt.getMonth() + 1;
+    const d = dt.getDate();
+    const formattedDate = `${y}-${m}-${d}`;
 
-	var marker = new google.maps.Marker({
-		position: myLatLng,
-		map: map,
-		title: name
-	});
+    const marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: `${name}\n${address}\n${formattedDate}`
+    });
 
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(contentString);
-		infowindow.open(map, marker);
-	});
+    google.maps.event.addListener(marker, 'click', () => {
+        infowindow.setContent(`<div><strong>${name}</strong><br>${address}<br>${formattedDate}</div>`);
+        infowindow.open(map, marker);
+    });
+
+    return marker;
 }
